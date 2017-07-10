b5bf813d19a64e7ba0899ee51a3325638bc169f7
schornakj <joe.schornak@gmail.com>
2017-07-10 17:58:44 -0500

    Added marker for volume pose

--- yak/src/ros/volume_tf_broadcaster.cpp
+++ yak/src/ros/volume_tf_broadcaster.cpp
@@ -5,9 +5,7 @@
 void processFeedback(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
 {
-  ROS_INFO_STREAM( feedback->marker_name << " is now at "
-      << feedback->pose.position.x << ", " << feedback->pose.position.y
-      << ", " << feedback->pose.position.z );
+  ROS_INFO_STREAM( feedback->marker_name << " is now at " << feedback->pose.position.x << ", " << feedback->pose.position.y << ", " << feedback->pose.position.z );
 }
 
 int main(int argc, char** argv) {
@@ -17,64 +15,63 @@ int main(int argc, char** argv) {
   tf::TransformBroadcaster br;
   tf::Transform transform;
 
-//  // create an interactive marker server on the topic namespace simple_marker
-//  interactive_markers::InteractiveMarkerServer server("simple_marker");
+  // create an interactive marker server on the topic namespace simple_marker
+  interactive_markers::InteractiveMarkerServer server("joe_simple_marker");
 
-//  // create an interactive marker for our server
-//  visualization_msgs::InteractiveMarker int_marker;
-//  int_marker.header.frame_id = "volume_link";
-//  int_marker.header.stamp=ros::Time::now();
-//  int_marker.name = "my_marker";
-//  int_marker.description = "Simple 1-DOF Control";
+  // create an interactive marker for our server
+  visualization_msgs::InteractiveMarker int_marker;
+  int_marker.header.frame_id = "volume_link";
+  int_marker.header.stamp=ros::Time::now();
+  int_marker.name = "my_marker";
+  int_marker.description = "Simple 1-DOF Control";
 
-//  // create a grey box marker
-//  visualization_msgs::Marker box_marker;
-//  box_marker.type = visualization_msgs::Marker::CUBE;
-//  box_marker.scale.x = 0.45;
-//  box_marker.scale.y = 0.45;
-//  box_marker.scale.z = 0.45;
-//  box_marker.color.r = 0.5;
-//  box_marker.color.g = 0.5;
-//  box_marker.color.b = 0.5;
-//  box_marker.color.a = 1.0;
+  // create a grey box marker
+  visualization_msgs::Marker box_marker;
+  box_marker.type = visualization_msgs::Marker::CUBE;
+  box_marker.scale.x = 0.45;
+  box_marker.scale.y = 0.45;
+  box_marker.scale.z = 0.45;
+  box_marker.color.r = 0.5;
+  box_marker.color.g = 0.5;
+  box_marker.color.b = 0.5;
+  box_marker.color.a = 1.0;
 
-//  // create a non-interactive control which contains the box
-//  visualization_msgs::InteractiveMarkerControl box_control;
-//  box_control.always_visible = true;
-//  box_control.markers.push_back( box_marker );
+  // create a non-interactive control which contains the box
+  visualization_msgs::InteractiveMarkerControl box_control;
+  box_control.always_visible = true;
+  box_control.markers.push_back( box_marker );
 
-//  // add the control to the interactive marker
-//  int_marker.controls.push_back( box_control );
+  // add the control to the interactive marker
+  int_marker.controls.push_back( box_control );
 
-//  // create a control which will move the box
-//  // this control does not contain any markers,
-//  // which will cause RViz to insert two arrows
-//  visualization_msgs::InteractiveMarkerControl rotate_control;
-//  rotate_control.name = "move_x";
-//  rotate_control.interaction_mode =
-//      visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+  // create a control which will move the box
+  // this control does not contain any markers,
+  // which will cause RViz to insert two arrows
+  visualization_msgs::InteractiveMarkerControl rotate_control;
+  rotate_control.name = "move_x";
+  rotate_control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
 
-//  // add the control to the interactive marker
-//  int_marker.controls.push_back(rotate_control);
+  // add the control to the interactive marker
+  int_marker.controls.push_back(rotate_control);
 
-//  // add the interactive marker to our collection &
-//  // tell the server to call processFeedback() when feedback arrives for it
-//  server.insert(int_marker, &processFeedback);
+  // add the interactive marker to our collection &
+  // tell the server to call processFeedback() when feedback arrives for it
+  server.insert(int_marker, &processFeedback);
 
-//  // 'commit' changes and send to all clients
-//  server.applyChanges();
+  // 'commit' changes and send to all clients
+  server.applyChanges();
 
-  ros::Rate rate(10.0);
+//  ros::Rate rate(10.0);
 
-  while (nh.ok()) {
-    transform.setOrigin((tf::Vector3(0.0, 0.0, 0.0)));
+//  while (nh.ok()) {
+//    transform.setOrigin((tf::Vector3(0.0, 0.0, 0.0)));
 
-    transform.setRotation(tf::Quaternion(tf::Vector3(0,0,1), tfScalar(1.5708)));
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "volume_frame"));
+//    transform.setRotation(tf::Quaternion(tf::Vector3(0,0,1), tfScalar(1.5708)));
+//    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "volume_frame"));
 
-    rate.sleep();
-  }
+//    rate.sleep();
+//  }
 
   ros::spin();
-  return 0;
+//  return 0;
 }
