8e617cb26df1178379f4312068f3969b1ae5c1e3
Joseph Schornak <joe.schornak@gmail.com>
2017-07-12 11:41:48 -0500

    Fixed volume not clearing between meshing calls

--- yak_meshing/src/yak_meshing_node.cpp
+++ yak_meshing/src/yak_meshing_node.cpp
@@ -32,7 +32,7 @@ public:
     tsdf_client_ = nh.serviceClient<yak::GetTSDF>("/kinfu/get_tsdf");
     tsdf_sparse_client_ = nh.serviceClient<yak::GetSparseTSDF>("/kinfu/get_sparse_tsdf");
     mesh_server_ = nh.advertiseService("get_mesh", &GenerateMesh::GetMesh, this);
-    grid = openvdb::FloatGrid::create(/*background value=*/1.0);
+    //grid_ = openvdb::FloatGrid::create(/*background value=*/1.0);
     if (!nh.param<std::string>("/meshing/output_path", mesh_output_path_, "/home/jschornak/meshes")) {
         ROS_INFO("Couldn't load output path parameter. Using default path.");
     }
@@ -76,7 +76,7 @@ public:
     //GenerateMesh::MakePointCloud(cloud, tsdf_data, size_x, size_y, size_z, num_voxels_x, num_voxels_y, num_voxels_z);
     ROS_INFO("Building voxel volume from serialized sparse data...");
     //GenerateMesh::MakeVoxelGrid(grid, tsdf_data, size_x, size_y, size_z, num_voxels_x, num_voxels_y, num_voxels_z);
-    GenerateMesh::MakeVoxelGridFromSparse(grid, tsdf_data, row_data, col_data, sheet_data, size_x, size_y, size_z, num_voxels_x, num_voxels_y, num_voxels_z);
+    GenerateMesh::MakeVoxelGridFromSparse(grid_, tsdf_data, row_data, col_data, sheet_data, size_x, size_y, size_z, num_voxels_x, num_voxels_y, num_voxels_z);
 
 //    ROS_INFO("Making an example sphere...");
 //    GenerateMesh::MakeSphereVoxelGrid(grid);
@@ -84,7 +84,7 @@ public:
 
     ROS_INFO("Meshing voxel volume...");
     openvdb::tools::VolumeToMesh mesher;
-    mesher.operator()<openvdb::FloatGrid>( grid.operator*() );
+    mesher.operator()<openvdb::FloatGrid>( grid_.operator*() );
     ROS_INFO("Done meshing volume");
     std::string path = mesh_output_path_ + "/new_mesh.obj";
     ROS_INFO_STREAM("Saving .obj to file at " << path << " ...");
@@ -143,6 +143,9 @@ public:
     bool MakeVoxelGridFromSparse(openvdb::FloatGrid::Ptr& grid, std::vector<unsigned int>& data, std::vector<uint16_t>& rowIndex, std::vector<uint16_t>& colIndex, std::vector<uint16_t>& sheetIndex, float size_x, float size_y, float size_z, int res_x, int res_y, int res_z) {
       typedef typename openvdb::FloatGrid::ValueType ValueT;
 
+      // Initialize grid with background value corresponding to "outside" estimated surface to clear existing data
+      grid_ = openvdb::FloatGrid::create(/*background value=*/1.0);
+
       openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
 
       float voxel_dim_x = (float)size_x/(float)res_x;
@@ -164,14 +167,10 @@ public:
         k = sheetIndex[a];
 
         uint32_t currentData = data[a];
-        //uint32_t currentData = data[res_y*res_z*k + res_y*j + i];
         half_float::half currentValue;
         uint16_t currentWeight;
-        //ROS_INFO("Getting TSDF data");
         GetTSDFData(currentData, currentValue, currentWeight);
         ValueT val = ValueT(currentValue);
-        //ROS_INFO("Setting OpenVBD voxel");
-        //ROS_INFO_STREAM((float)currentValue <<", " << currentWeight << ", " << "["<<i<<","<<j<<","<<k<<"]");
         accessor.setValue(ijk, val);
       }
       ROS_INFO("Scaling voxel volume");
@@ -183,7 +182,7 @@ public:
     bool MakeSphereVoxelGrid(openvdb::FloatGrid::Ptr& grid) {
       grid = openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(1.0, openvdb::Vec3f(0,0,0), /*voxel size=*/0.5, /*width=*/4.0);
     }
-    openvdb::FloatGrid::Ptr grid;
+    openvdb::FloatGrid::Ptr grid_;
 
 private:
   void WriteMesh(const char* filename,
