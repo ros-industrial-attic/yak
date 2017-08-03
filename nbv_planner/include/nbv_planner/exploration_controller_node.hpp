318f19745b5614bb59635adc4979a857fb39bc05
schornakj <joe.schornak@gmail.com>
2017-08-03 16:35:48 -0500

    Added motion planning constraints to keep camera pointed towards objects during reconstruction

--- nbv_planner/include/nbv_planner/exploration_controller_node.hpp
+++ nbv_planner/include/nbv_planner/exploration_controller_node.hpp
@@ -5,10 +5,20 @@
 #include <nbv_planner/GetNBVResponse.h>
 
 #include <tf/tf.h>
+#include <tf/transform_listener.h>
 
 #include <std_srvs/Empty.h>
 #include <std_srvs/EmptyRequest.h>
 #include <std_srvs/EmptyResponse.h>
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_pipeline/planning_pipeline.h>
+#include <moveit_msgs/Constraints.h>
+#include <moveit/kinematic_constraints/kinematic_constraint.h>
+#include <moveit/kinematic_constraints/utils.h>
+
+#include <shape_msgs/SolidPrimitive.h>
 
 class Explorer {
 public:
@@ -22,6 +32,9 @@ public:
 
 //  moveit::planning_interface::MoveGroupInterface move_group_;
   ros::ServiceClient nbv_client_;
+  tf::TransformListener tfListener_;
+  ros::NodeHandle node_handle_;
+
 //  ros::ServiceServer exploration_server_;
 
 private:
