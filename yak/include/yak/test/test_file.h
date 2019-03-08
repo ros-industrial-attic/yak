b8e56291d1362f931a1b11ccfa421059a35d591e
Joseph G. Schornak <joseph.schornak@swri.org>
2019-03-08 17:24:15 -0600

    CMakeLists cleanup

--- yak/include/yak/test/test_file.h
+++
@@ -1,9 +0,0 @@
-#ifndef YAK_TEST_FILE_H
-#define YAK_TEST_FILE_H
-namespace yak
-{
-void sayHello();
-}
-#endif // YAK_TEST_FILE_H
-
-
--- yak/src/test/tsdf_tests.cpp
+++ yak/src/test/tsdf_tests.cpp
@@ -1,14 +1,12 @@
 #include <yak/ros/yak_server.h>
-//#include <yak/mc/marching_cubes.h>
-#include <yak/test/test_file.h>
+#include <yak/mc/marching_cubes.h>
 
 int main()
 {
     kfusion::KinFuParams default_params = kfusion::KinFuParams::default_params();
     Eigen::Affine3f world_to_volume (Eigen::Affine3f::Identity());
-//    yak::MarchingCubesParameters mc_params;
+    yak::MarchingCubesParameters mc_params;
     yak::FusionServer fusion(default_params, world_to_volume);
 
-    yak::sayHello();
     return 0;
 }
